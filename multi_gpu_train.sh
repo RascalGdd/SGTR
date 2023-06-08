658e0579dde0d2499db1e8a719d57213032a79a5
RascalGdd <98754472+RascalGdd@users.noreply.github.com>
2023-06-08 09:54:13 +0200

    Add files via upload

---
+++ multi_gpu_train.sh
@@ -0,0 +1,13 @@
+# move to experiment directory
+cd playground/sgg/detr.res101.c5.one_stage_rel_tfmer
+# link the config file
+#rm config.py; ln -s config_vg_sgtr.py config.py
+
+pods_train --num-gpus 2
+
+
+
+
+
+
+
