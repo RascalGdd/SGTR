becaaef1c8c09f6dd183127d4472d20ced9f4a1b
drlxj <xuejingluo98@gmail.com>
2023-06-09 14:09:44 +0200

    run for experiment: /cluster/work/cvl/denfan/diandian/SGTR/CVPODS_output/2023-06-09_14-09-4dor-SGTR-rel_dec-6

--- cvpods/evaluation/sgg_oi_evaluation.py
+++ cvpods/evaluation/sgg_oi_evaluation.py
@@ -604,10 +604,6 @@ def oi_sgg_evaluation(all_results, predicate_cls_list, result_str, logger, post_
     empty_cate = ''
     for c in tqdm(range(len(predicate_cls_list_woBG))):
         rec, prec, ap = ap_eval(cls_image_ids[c], cls_dets[c], cls_gts[c], npos[c], True)
-        print(rec.shape)
-        print(prec.shape)
-        print(rec)
-        print(prec)
 
         if ap is None:
             empty_cate += f"{c}-{predicate_cls_list_woBG[c]}, "
