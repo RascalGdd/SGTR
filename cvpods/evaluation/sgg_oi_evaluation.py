ac102cfdc12e60389a25a59778f07abc7e7dd4d6
Diandian Guo <guodiandian1998@163.com>
2023-06-30 12:59:25 +0200

    debug, add nms

--- cvpods/evaluation/sgg_oi_evaluation.py
+++ cvpods/evaluation/sgg_oi_evaluation.py
@@ -463,7 +463,7 @@ def oi_sgg_evaluation(all_results, predicate_cls_list, result_str, logger, post_
             preds = np.asarray(filtered_preds)
 
             filtered_preds_list = [list(item) for item in filtered_preds]
-            filtered_classes_list = [list(item) for item in filtered_classes]
+            # filtered_classes_list = [list(item) for item in filtered_classes]
 
             one2one = True
 
@@ -1071,3 +1071,33 @@ def _compute_pred_matches(gt_triplets, pred_triplets,
         for i in np.where(keep_inds)[0][inds]:
             pred_to_gt[i].append(int(gt_ind))
     return pred_to_gt
+
+
+def nms_cpu(dets, thresh):
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    scores = dets[:, 4]
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    order = scores.argsort()[::-1]
+
+    keep = []
+    while order.size > 0:
+        i = order.item(0)
+        keep.append(i)
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        inds = np.where(ovr <= thresh)[0]
+        order = order[inds + 1]
+
+    return keep
