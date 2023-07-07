d562dba8f2da9f4f077a5a7cfe0b10cfa1b15c1e
Diandian Guo <guodiandian1998@163.com>
2023-07-07 15:55:43 +0200

    debug

--- cvpods/evaluation/sgg_oi_evaluation.py
+++ cvpods/evaluation/sgg_oi_evaluation.py
@@ -491,17 +491,17 @@ def oi_sgg_evaluation(all_results, predicate_cls_list, result_str, logger, post_
             rel_trp_prd_scores = res['prd_trp_score']
             # pred_rel_pair_idxs = res['pred_rel_pair_idxs']
 
-            print("rel_prd_labels")
-            print(rel_prd_labels.shape)
-            print(rel_prd_score.shape)
-            print(rel_trp_prd_scores.shape)
-            print(rel_prd_score_dist.shape)
+            # print("rel_prd_labels")
+            # print(rel_prd_labels.shape)
+            # print(rel_prd_score.shape)
+            # print(rel_trp_prd_scores.shape)
+            # print(rel_prd_score_dist.shape)
             # rel_det_scores_prd = rel_prd_score_dist[:, 1:]  # N x C (the prediction score of each categories)
             # print(np.argsort(-rel_prd_score[:, 1:], axis=1))
             # print("compare 1")
             # print(np.unique(rel_prd_labels - np.argsort(-rel_prd_score[:, 1:], axis=1)))
             print("compare")
-            print(np.unique(rel_prd_labels - np.argsort(-rel_prd_score_dist[:, 1:], axis=0)))
+            print(np.unique(rel_prd_labels - np.argmax(rel_prd_score_dist[:, 1:], axis=1)))
             # print("compare 3")
             # print(np.unique(rel_prd_labels - np.argsort(-rel_trp_prd_scores[:, 1:], axis=1)))
 
